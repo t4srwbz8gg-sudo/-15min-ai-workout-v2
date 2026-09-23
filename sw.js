c313a94772c51cda5478978740f1c28ffdd2f3c6
t4srwbz8gg-sudo <t4srwbz8gg@privaterelay.appleid.com>
2026-09-23 14:41:30 +0900

    Use network-first navigation for V2 updates

--- sw.js
+++ sw.js
@@ -1,1 +1,21 @@
-const C='ai15-v2-1',A=['./','./index.html','./manifest.webmanifest','./icon.svg'];self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(A)));self.skipWaiting()});self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))));self.clients.claim()});self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(s=>{const x=s.clone();caches.open(C).then(c=>c.put(e.request,x));return s}).catch(()=>caches.match('./index.html'))))});
+const C='ai15-v2-4',A=['./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(C).then(c=>c.addAll(A)));
+  self.skipWaiting();
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  if(e.request.mode==='navigate'){
+    e.respondWith(fetch(e.request).then(r=>{
+      const copy=r.clone();caches.open(C).then(c=>c.put('./index.html',copy));return r;
+    }).catch(()=>caches.match('./index.html')));
+    return;
+  }
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
+    const copy=res.clone();caches.open(C).then(c=>c.put(e.request,copy));return res;
+  })));
+});
